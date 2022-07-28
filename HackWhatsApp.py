9c69f12ac068302ee3ecb40afdad328d4c05341d
Alpha-hack2022 <110197265+Alpha-hack2022@users.noreply.github.com>
2022-07-29 00:21:06 +0430

    Update and rename README.md to HackWhatsApp.py

---
+++ HackWhatsApp.py
@@ -0,0 +1,11 @@
+# HackWhatsApp
+HackWhatsApp
+apt udate
+apt upgrade
+pkg install git
+pkg install python
+git clone https://github.com/Alpha-hack2022/HackWhatsApp
+ls
+cd HackWhatsApp
+ls
+python HackWhatsApp.py
--- README.md
+++
@@ -1,2 +0,0 @@
-# HackWhatsApp
-HackWhatsApp
